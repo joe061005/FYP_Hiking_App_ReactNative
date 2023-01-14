ac0a1f12a5d4437ebb811f462bd26ab7252215ba
joe061005 <72422823+joe061005@users.noreply.github.com>
2023-01-14 23:11:10 +0800

    change the baseURL

--- Api/api.js
+++ Api/api.js
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import moment from 'moment'
 
 const api_path = {
-    baseURL: 'https://ea8a-223-19-143-35.ngrok.io/'
+    baseURL: 'https://hiking-app-backend.azurewebsites.net/'
 }
 
 let date = moment().format('YYYY-MM-DD');
